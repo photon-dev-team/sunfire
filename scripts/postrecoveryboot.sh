91ccf634a6e2cdcdeeaab602eb1a16b2d7b2d0ce
justin <jokersax@gmail.com>
2012-02-01 13:45:21 -0500

    various fixes and cleaning up a little

--- scripts/postrecoveryboot.sh
+++ scripts/postrecoveryboot.sh
@@ -1,3 +1,12 @@
 #!/sbin/sh
 
+# enable ADB
 echo msc_adb > /dev/usb_device_mode
+
+# upload firmware to touch screen
+sleep 5 #wait for /dev/block/mmcblk0p12 to appear
+mount /dev/block/mmcblk0p12 /system
+cp -a /system/etc/touchpad /etc/
+/system/bin/touchpad -a
+umount /system
+rm /etc/touchpad -Rf
