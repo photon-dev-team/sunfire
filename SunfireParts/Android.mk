7aad426505913bedfa7882fd485c09716e38fb0d
justin <jokersax@gmail.com>
2012-02-11 11:50:01 -0500

    opps

--- SunfireParts/Android.mk
+++ SunfireParts/Android.mk
@@ -7,7 +7,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := PhotonParts
+LOCAL_PACKAGE_NAME := SunfireParts
 LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
