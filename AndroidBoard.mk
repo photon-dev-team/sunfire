3d3d16a35be35d3dcb0d9d41847244ad009f503f
justin <jokersax@gmail.com>
2012-04-27 00:02:34 -0400

    finally

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -31,14 +31,14 @@ include $(CLEAR_VARS)
 TARGET_KERNEL_CONFIG := tegra_sunfire_cyanogenmod_defconfig
 TARGET_PREBUILT_KERNEL := device/moto/sunfire/kernel
 
-OLYMPUS_WIFI_MODULE:
+SUNFIRE_WIFI_MODULE:
 	make -C kernel/moto/olympus/wifi-module/wlan/osrc/open-src/src/dhd/linux/ \
 	ARCH="arm" CROSS_COMPILE="arm-eabi-" LINUXSRCDIR=~/cm9/kernel/moto/sunfire/ \
 	LINUXBUILDDIR=$(KERNEL_OUT) PLATFORM_DIR=~/cm9 \
 	KERNEL_SRC=$PLATFORM_DIR/kernel/moto/sunfire ANDROID_BUILD_TOP=/$PLATFORM_DIR \
 	BCM_INSTALLDIR="$(ANDROID_BUILD_TOP)/$(KERNEL_MODULES_OUT)"
 
-TARGET_KERNEL_MODULES := OLYMPUS_WIFI_MODULE
+TARGET_KERNEL_MODULES := SUNFIRE_WIFI_MODULE
 
 
 # include the non-open-source counterpart to this file
